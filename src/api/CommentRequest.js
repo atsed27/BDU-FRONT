a3e2ec4677f1443039caad93e5fa5d91d051c456
Dani <you@example.com>
2023-06-12 20:54:58 -0300

    Api add

--- src/api/CommentRequest.js
+++ src/api/CommentRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://bdu-app-api.onrender.com' });
 
 export const createComment = (postId,data) => API.post(`/comment/${postId}/create`, data);
 export const updateComment = (comId, data) => API.put(`/comment/${comId}/update`, data);
